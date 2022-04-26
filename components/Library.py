42148dbb69808a2db9b91d1eac3fa53893f65faf
Marek Małek <mmalek06@gmail.com>
2022-04-26 21:04:43 +0200

    moving helper function code outside of the main module

--- components/Library.py
+++ components/Library.py
@@ -1,9 +1,8 @@
 import typing
 
-from functools import lru_cache
-
-from models import Author, User, Book
 from errors import BookBorrowedError, BookNotFoundError
+from functions import lev_dist
+from models import Author, User, Book
 
 
 class Library:
@@ -21,7 +20,7 @@ class Library:
 
         for book in self.__books:
             for author in book.authors:
-                distance = Library.__lev_dist(author.name, maybe_author_name)
+                distance = lev_dist(author.name, maybe_author_name)
 
                 if distance > self.__author_search_sensitivity:
                     continue
@@ -36,7 +35,7 @@ class Library:
         candidates = []
 
         for book in self.__books:
-            distance = Library.__lev_dist(book.title.lower(), maybe_title.lower())
+            distance = lev_dist(book.title.lower(), maybe_title.lower())
 
             if distance > self.__book_search_sensitivity:
                 continue
@@ -70,41 +69,3 @@ class Library:
             return book
 
         raise BookNotFoundError(f'A book with isbn = {isbn} not found.')
-
-    @staticmethod
-    def __lev_dist(a: str, b: str) -> int:
-        '''
-        This function will calculate the levenshtein distance between two input
-        strings a and b
-
-        params:
-            a (String) : The first string you want to compare
-            b (String) : The second string you want to compare
-
-        returns:
-            This function will return the distnace between string a and b.
-
-        example:
-            a = 'stamp'
-            b = 'stomp'
-            lev_dist(a,b)
-            >> 1.0
-        '''
-
-        @lru_cache(None)  # for memorization
-        def min_dist(s1: int, s2: int) -> int:
-            if s1 == len(a) or s2 == len(b):
-                return len(a) - s1 + len(b) - s2
-
-            # no change required
-            if a[s1] == b[s2]:
-                return min_dist(s1 + 1, s2 + 1)
-
-            return 1 + min(
-                min_dist(s1, s2 + 1),  # insert character
-                min_dist(s1 + 1, s2),  # delete character
-                min_dist(s1 + 1, s2 + 1),  # replace character
-            )
-
-        return min_dist(0, 0)
-
