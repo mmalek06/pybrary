54a187b50aaab3981c9966efd4db66ea24770b2f
Marek Małek <mmalek06@gmail.com>
2022-05-08 12:33:36 +0200

    added default lib file

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
+from argparse import ArgumentParser
+
 from app.core.io import ConsoleInput, ConsoleOutput, read_library
 from app.core.Library import Library
 from app.core.enums import ActionType
 from app.ui.UI import UI
 
 
-books = read_library('data/default.tsv')
+parser = ArgumentParser()
+
+parser.add_argument(
+    '-f',
+    '--file',
+    default='data/default.tsv',
+    dest='filename',
+    help='pick a custom library filename',
+    metavar='FILE')
+
+args = parser.parse_args()
+books = read_library(args.filename)
 lib = Library(books)
 cinput = ConsoleInput()
 output = ConsoleOutput()
